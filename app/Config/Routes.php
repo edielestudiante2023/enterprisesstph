77d84e861858dbbc7b729fdd11a420e78c8ef8b7
edielestudiante2023 <edielestudiante2023@gmail.com>
2025-02-11 20:29:32 -0500

    reestableciendo pta cliente a 2 rutas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -492,10 +492,7 @@ $routes->get('vencimientos/send-emails', 'VencimientosMantenimientoController::s
 $routes->get('/listVencimientosCliente/(:num)', 'VencimientosClienteController::listVencimientosCliente/$1');
 
 $routes->get('api/getClientes', 'PlanDeTrabajoAnualController::getClientes');
-$routes->get('api/getActividadesAjax', 'PlanDeTrabajoAnualController::getActividadesAjax');
-$routes->post('api/updatePlanDeTrabajo', 'PlanDeTrabajoAnualController::updatePlanDeTrabajo');
-$routes->get('listPlanDeTrabajoAnualAjax', 'PlanDeTrabajoAnualController::listPlanDeTrabajoAnualAjax');
-
+$routes->get('api/getActividades', 'PlanDeTrabajoAnualController::getActividades');
 
 
 $routes->get('api/getClientes', 'EvaluationController::getClientes');
