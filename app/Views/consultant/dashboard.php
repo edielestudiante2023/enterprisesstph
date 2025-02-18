c7bdbcf6ae7f7a5b70a8326ae02ba3ffc04f3bd6
edielestudiante2023 <edielestudiante2023@gmail.com>
2025-02-17 19:15:12 -0500

    favicon

--- app/Views/consultant/dashboard.php
+++ app/Views/consultant/dashboard.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dashboard Consultor 2025</title>
-    <link rel="icon" href="<?= base_url('path/to/favicon.ico') ?>" type="image/x-icon">
+    <link rel="icon" href="<?= base_url('favicon.ico') ?>" type="image/x-icon">
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
     <!-- Bootstrap CSS -->
